466239c8cb0b62e166b23a4c3e8be1922d87b088
Ahmed ElKerdany <akerdany@gmail.com>
2019-02-26 21:58:41 +0200

    DB

--- Database.sql
+++ Database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Feb 21, 2019 at 05:32 PM
+-- Generation Time: Feb 26, 2019 at 08:57 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.2.10
 
@@ -57,6 +57,14 @@ CREATE TABLE `ccd` (
   `courtDetailsId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `ccd`
+--
+
+INSERT INTO `ccd` (`id`, `courtId`, `courtDetailsId`) VALUES
+(1, 1, 1),
+(2, 2, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -70,16 +78,32 @@ CREATE TABLE `court` (
   `price` int(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `court`
+--
+
+INSERT INTO `court` (`id`, `courtNumber`, `sportId`, `price`) VALUES
+(1, '1', 1, 50),
+(2, '2', 1, 70);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `courtDetails`
+-- Table structure for table `courtdetails`
 --
 
-CREATE TABLE `courtDetails` (
+CREATE TABLE `courtdetails` (
   `id` int(11) NOT NULL,
   `specs` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `courtdetails`
+--
+
+INSERT INTO `courtdetails` (`id`, `specs`) VALUES
+(1, 'Clay'),
+(2, 'Grass');
 
 -- --------------------------------------------------------
 
@@ -108,6 +132,26 @@ CREATE TABLE `features` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `options`
+--
+
+CREATE TABLE `options` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `type` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `options`
+--
+
+INSERT INTO `options` (`id`, `name`, `type`) VALUES
+(1, 'Name', 'text'),
+(2, 'expDate', 'date');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `pages`
 --
 
@@ -132,6 +176,30 @@ INSERT INTO `pages` (`id`, `link`, `pageName`, `userTypeId`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `paymentMethod`
+--
+
+CREATE TABLE `paymentMethod` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `paymentMethodOptions`
+--
+
+CREATE TABLE `paymentMethodOptions` (
+  `id` int(11) NOT NULL,
+  `paymentId` int(11) NOT NULL,
+  `optionId` int(11) NOT NULL,
+  `priority` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `previliges`
 --
 
@@ -139,6 +207,19 @@ CREATE TABLE `previliges` (
   `id` int(11) NOT NULL,
   `userTypeId` int(11) NOT NULL,
   `featureId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `P_Method_Option_Value`
+--
+
+CREATE TABLE `P_Method_Option_Value` (
+  `id` int(11) NOT NULL,
+  `paymentMethodId` int(11) NOT NULL,
+  `value` text NOT NULL,
+  `reservationId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -154,13 +235,20 @@ CREATE TABLE `reservation` (
   `reservationDetailsId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `reservation`
+--
+
+INSERT INTO `reservation` (`id`, `userId`, `courtId`, `reservationDetailsId`) VALUES
+(1, 3, 1, 1);
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `reservationDetails`
+-- Table structure for table `reservationdetails`
 --
 
-CREATE TABLE `reservationDetails` (
+CREATE TABLE `reservationdetails` (
   `id` int(11) NOT NULL,
   `date` date NOT NULL,
   `startTime` time NOT NULL,
@@ -169,6 +257,13 @@ CREATE TABLE `reservationDetails` (
   `type` varchar(100) NOT NULL,
   `cost` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `reservationdetails`
+--
+
+INSERT INTO `reservationdetails` (`id`, `date`, `startTime`, `endTime`, `supervisorId`, `type`, `cost`) VALUES
+(1, '2019-02-11', '02:00:00', '04:00:00', 4, 'player', 50);
 
 -- --------------------------------------------------------
 
@@ -180,6 +275,13 @@ CREATE TABLE `sports` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `sports`
+--
+
+INSERT INTO `sports` (`id`, `name`) VALUES
+(1, 'Tennis');
 
 -- --------------------------------------------------------
 
@@ -213,19 +315,19 @@ INSERT INTO `user` (`id`, `firstName`, `lastName`, `email`, `password`, `dateOfB
 -- --------------------------------------------------------
 
 --
--- Table structure for table `userType`
+-- Table structure for table `usertype`
 --
 
-CREATE TABLE `userType` (
+CREATE TABLE `usertype` (
   `id` int(11) NOT NULL,
   `userTypeName` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `userType`
+-- Dumping data for table `usertype`
 --
 
-INSERT INTO `userType` (`id`, `userTypeName`) VALUES
+INSERT INTO `usertype` (`id`, `userTypeName`) VALUES
 (1, 'Admin'),
 (2, 'Guest'),
 (3, 'Test'),
@@ -258,9 +360,9 @@ ALTER TABLE `court`
   ADD KEY `sportId` (`sportId`);
 
 --
--- Indexes for table `courtDetails`
+-- Indexes for table `courtdetails`
 --
-ALTER TABLE `courtDetails`
+ALTER TABLE `courtdetails`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -276,11 +378,31 @@ ALTER TABLE `features`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `options`
+--
+ALTER TABLE `options`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `pages`
 --
 ALTER TABLE `pages`
   ADD PRIMARY KEY (`id`),
   ADD KEY `userTypeId` (`userTypeId`);
+
+--
+-- Indexes for table `paymentMethod`
+--
+ALTER TABLE `paymentMethod`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `paymentMethodOptions`
+--
+ALTER TABLE `paymentMethodOptions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `paymentId` (`paymentId`),
+  ADD KEY `optionId` (`optionId`);
 
 --
 -- Indexes for table `previliges`
@@ -289,6 +411,14 @@ ALTER TABLE `previliges`
   ADD PRIMARY KEY (`id`),
   ADD KEY `userTypeId` (`userTypeId`),
   ADD KEY `optionId` (`featureId`);
+
+--
+-- Indexes for table `P_Method_Option_Value`
+--
+ALTER TABLE `P_Method_Option_Value`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `paymentMethodId` (`paymentMethodId`),
+  ADD KEY `reservationId` (`reservationId`);
 
 --
 -- Indexes for table `reservation`
@@ -300,9 +430,9 @@ ALTER TABLE `reservation`
   ADD KEY `userId` (`userId`);
 
 --
--- Indexes for table `reservationDetails`
+-- Indexes for table `reservationdetails`
 --
-ALTER TABLE `reservationDetails`
+ALTER TABLE `reservationdetails`
   ADD PRIMARY KEY (`id`),
   ADD KEY `supervisorId` (`supervisorId`);
 
@@ -321,9 +451,9 @@ ALTER TABLE `user`
   ADD KEY `address_id` (`addressId`);
 
 --
--- Indexes for table `userType`
+-- Indexes for table `usertype`
 --
-ALTER TABLE `userType`
+ALTER TABLE `usertype`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -340,19 +470,19 @@ ALTER TABLE `address`
 -- AUTO_INCREMENT for table `ccd`
 --
 ALTER TABLE `ccd`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `court`
 --
 ALTER TABLE `court`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
--- AUTO_INCREMENT for table `courtDetails`
+-- AUTO_INCREMENT for table `courtdetails`
 --
-ALTER TABLE `courtDetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `courtdetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `events`
@@ -367,10 +497,28 @@ ALTER TABLE `features`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
+-- AUTO_INCREMENT for table `options`
+--
+ALTER TABLE `options`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `pages`
 --
 ALTER TABLE `pages`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `paymentMethod`
+--
+ALTER TABLE `paymentMethod`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `paymentMethodOptions`
+--
+ALTER TABLE `paymentMethodOptions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `previliges`
@@ -379,22 +527,28 @@ ALTER TABLE `previliges`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `reservation`
+-- AUTO_INCREMENT for table `P_Method_Option_Value`
 --
-ALTER TABLE `reservation`
+ALTER TABLE `P_Method_Option_Value`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `reservationDetails`
+-- AUTO_INCREMENT for table `reservation`
 --
-ALTER TABLE `reservationDetails`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `reservation`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `reservationdetails`
+--
+ALTER TABLE `reservationdetails`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `sports`
 --
 ALTER TABLE `sports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -403,9 +557,9 @@ ALTER TABLE `user`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `userType`
+-- AUTO_INCREMENT for table `usertype`
 --
-ALTER TABLE `userType`
+ALTER TABLE `usertype`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
@@ -422,7 +576,7 @@ ALTER TABLE `address`
 -- Constraints for table `ccd`
 --
 ALTER TABLE `ccd`
-  ADD CONSTRAINT `ccd_ibfk_1` FOREIGN KEY (`courtDetailsId`) REFERENCES `courtDetails` (`id`),
+  ADD CONSTRAINT `ccd_ibfk_1` FOREIGN KEY (`courtDetailsId`) REFERENCES `courtdetails` (`id`),
   ADD CONSTRAINT `ccd_ibfk_2` FOREIGN KEY (`courtId`) REFERENCES `court` (`id`);
 
 --
@@ -435,14 +589,28 @@ ALTER TABLE `court`
 -- Constraints for table `pages`
 --
 ALTER TABLE `pages`
-  ADD CONSTRAINT `pages_ibfk_1` FOREIGN KEY (`userTypeId`) REFERENCES `userType` (`id`);
+  ADD CONSTRAINT `pages_ibfk_1` FOREIGN KEY (`userTypeId`) REFERENCES `usertype` (`id`);
+
+--
+-- Constraints for table `paymentMethodOptions`
+--
+ALTER TABLE `paymentMethodOptions`
+  ADD CONSTRAINT `paymentMethodOptions_ibfk_1` FOREIGN KEY (`optionId`) REFERENCES `options` (`id`),
+  ADD CONSTRAINT `paymentMethodOptions_ibfk_2` FOREIGN KEY (`paymentId`) REFERENCES `paymentMethod` (`id`);
 
 --
 -- Constraints for table `previliges`
 --
 ALTER TABLE `previliges`
-  ADD CONSTRAINT `previliges_ibfk_1` FOREIGN KEY (`userTypeId`) REFERENCES `userType` (`id`),
+  ADD CONSTRAINT `previliges_ibfk_1` FOREIGN KEY (`userTypeId`) REFERENCES `usertype` (`id`),
   ADD CONSTRAINT `previliges_ibfk_2` FOREIGN KEY (`featureId`) REFERENCES `features` (`id`);
+
+--
+-- Constraints for table `P_Method_Option_Value`
+--
+ALTER TABLE `P_Method_Option_Value`
+  ADD CONSTRAINT `P_Method_Option_Value_ibfk_1` FOREIGN KEY (`reservationId`) REFERENCES `reservation` (`id`),
+  ADD CONSTRAINT `P_Method_Option_Value_ibfk_2` FOREIGN KEY (`paymentMethodId`) REFERENCES `paymentMethod` (`id`);
 
 --
 -- Constraints for table `reservation`
@@ -450,19 +618,19 @@ ALTER TABLE `previliges`
 ALTER TABLE `reservation`
   ADD CONSTRAINT `reservation_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`),
   ADD CONSTRAINT `reservation_ibfk_2` FOREIGN KEY (`courtId`) REFERENCES `court` (`id`),
-  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`reservationDetailsId`) REFERENCES `reservationDetails` (`id`);
+  ADD CONSTRAINT `reservation_ibfk_3` FOREIGN KEY (`reservationDetailsId`) REFERENCES `reservationdetails` (`id`);
 
 --
--- Constraints for table `reservationDetails`
+-- Constraints for table `reservationdetails`
 --
-ALTER TABLE `reservationDetails`
+ALTER TABLE `reservationdetails`
   ADD CONSTRAINT `reservationDetails_ibfk_1` FOREIGN KEY (`supervisorId`) REFERENCES `user` (`id`);
 
 --
 -- Constraints for table `user`
 --
 ALTER TABLE `user`
-  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`userTypeId`) REFERENCES `userType` (`id`),
+  ADD CONSTRAINT `user_ibfk_1` FOREIGN KEY (`userTypeId`) REFERENCES `usertype` (`id`),
   ADD CONSTRAINT `user_ibfk_2` FOREIGN KEY (`addressId`) REFERENCES `address` (`id`);
 COMMIT;
 
