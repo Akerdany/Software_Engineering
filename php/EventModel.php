89054e5027599f8a7b4394750124ce4c0969fa26
Ahmed ElKerdany <akerdany@gmail.com>
2019-05-01 00:25:15 +0200

    errors

--- php/EventModel.php
+++ php/EventModel.php
@@ -1,18 +1,17 @@
 <?php
-require("connection.php");
-include_once('Icrud.php');
-class EventModel implements Icrud
-{
+require_once "connection.php";
+include_once 'Icrud.php';
+class EventModel implements Icrud {
     public $ID;
     public $Name;
     public $Date;
     public $Details;
     public $isDeleted;
-    public static function display()
-    {
-        $DB = new DbConnection();
-        $conn = $DB->getdbconnect();
-        $sql = 'SELECT id, name, date, details FROM events WHERE `isDeleted`= 0';
+    public static function display() {
+        // $DB = new DbConnection();
+        $DB     = DbConnection::getInstance();
+        $conn   = $DB->getdbconnect();
+        $sql    = 'SELECT id, name, date, details FROM events WHERE `isDeleted`= 0';
         $result = mysqli_query($DB->getdbconnect(), $sql);
         $event;
         $i = 0;
@@ -22,41 +21,41 @@ class EventModel implements Icrud
         }
         return $event;
     }
-    public static function add($E)
-    {
-        $DB = new DbConnection();
-        $conn=$DB->getdbconnect();
-        $Q="INSERT INTO `events` (`name`, `date`, `details`, `isDeleted`) VALUES ('$E->Name', '$E->Date', '$E->Details', '0')";
-        mysqli_query($conn,$Q);
+    public static function add($E) {
+        // $DB = new DbConnection();
+        $DB   = DbConnection::getInstance();
+        $conn = $DB->getdbconnect();
+        $Q    = "INSERT INTO `events` (`name`, `date`, `details`, `isDeleted`) VALUES ('$E->Name', '$E->Date', '$E->Details', '0')";
+        mysqli_query($conn, $Q);
     }
-    public static function getEventDetails($id)
-    {
-        $DB = new DbConnection();
-        $conn=$DB->getdbconnect();
-        $q = 'SELECT * from events WHERE isDeleted=0 AND id = '.$id;
-        $result = mysqli_query($conn, $q);
-        $row = mysqli_fetch_array($result);
-        $event = new EventModel();
-        $event->Name = $row['name'];
-        $event->Date = $row['date'];
+    public static function getEventDetails($id) {
+        // $DB = new DbConnection();
+        $DB             = DbConnection::getInstance();
+        $conn           = $DB->getdbconnect();
+        $q              = 'SELECT * from events WHERE isDeleted=0 AND id = ' . $id;
+        $result         = mysqli_query($conn, $q);
+        $row            = mysqli_fetch_array($result);
+        $event          = new EventModel();
+        $event->Name    = $row['name'];
+        $event->Date    = $row['date'];
         $event->Details = $row['details'];
         $event->ID      = $row['id'];
         return $event;
     }
-    public static function edit($E)
-    {
-        $DB = new DbConnection();
-        $conn=$DB->getdbconnect();
-        $Q="UPDATE `events` SET `name` = '$E->Name', `Date` = '$E->Date', `Details` = '$E->Details' WHERE `ID` = '$E->ID' ";
-        mysqli_query($conn,$Q);
+    public static function edit($E) {
+        // $DB = new DbConnection();
+        $DB   = DbConnection::getInstance();
+        $conn = $DB->getdbconnect();
+        $Q    = "UPDATE `events` SET `name` = '$E->Name', `Date` = '$E->Date', `Details` = '$E->Details' WHERE `ID` = '$E->ID' ";
+        mysqli_query($conn, $Q);
         header('Location: displayEvents.php');
     }
-    public static function delete($ID)
-    {
-        $DB = new DbConnection();
-        $conn=$DB->getdbconnect();
-        $Q="UPDATE `events` SET `isDeleted` = '1' WHERE `ID` = '$ID' ";
-        mysqli_query($conn,$Q);
+    public static function delete($ID) {
+        // $DB = new DbConnection();
+        $DB   = DbConnection::getInstance();
+        $conn = $DB->getdbconnect();
+        $Q    = "UPDATE `events` SET `isDeleted` = '1' WHERE `ID` = '$ID' ";
+        mysqli_query($conn, $Q);
         header('Location: displayEvents.php');
     }
 }
