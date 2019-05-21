5b6d7dc012d6d22b3bcdad4e6d3af020cb35d2b4
Youssef Wageeh <youssef1604941@miuegypt.edu.eg>
2019-05-22 00:13:54 +0200

    Hide pages when logged out Event + Court + PmOptions + PM

--- php/CourtView.php
+++ php/CourtView.php
@@ -11,6 +11,8 @@ class CourtView
         <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.18/b-1.5.6/datatables.min.css"/>
         <script type="text/javascript" src="https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.18/b-1.5.6/datatables.min.js"></script>';
         $user = new User();
+        if(!empty($_SESSION))
+        {
         echo '<div id="myDiv" style="width:70%">';
         echo '<table id="table_id" class = "table text-center table-dark table-striped table-hover table-bordered">';
             echo '<thead><tr>'
@@ -58,11 +60,13 @@ class CourtView
         $('#table_id').DataTable();
     } );
     </script>";
+
     if($user->getPermission('addcourt')){
             echo '<br><br><form action = "CourtController.php" method = "POST">';
             echo '<button type= "submit" name = "addButton">Add Court</button>'
                 .'</form>';
     }
+}
             
     }
     public static function Undisplay() {
